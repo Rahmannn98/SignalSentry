8002f4d6487587a5379c6dd5c1dfc2fed0f6ca37
Rahmannn <saifulloh.rahman028@student.unud.ac.id>
2026-06-12 17:30:26 +0700

    Update cardMission, make a responsif to dark and lightmode

--- SignalCentry/Components/GlassCardModifier.swift
+++ SignalCentry/Components/GlassCardModifier.swift
@@ -8,28 +8,50 @@
 import SwiftUI
 struct GlassCardModifier: ViewModifier {
     @Environment(\.colorSchemeContrast) var contrast
+    @Environment(\.colorScheme) var colorScheme
+    
+    var cornerRadius: CGFloat = 16
+    var isCapsule: Bool = false
     
     func body(content: Content) -> some View {
         content
             .padding(16)
-            .background(contrast == .increased ? Color.black : Color.white)
             .background {
-                if contrast != .increased {
-                    AnyView(Color.spaceDark.opacity(0.1))
-                }else{
-                    AnyView(EmptyView())
-                    }
+                if contrast == .increased {
+                    colorScheme == .dark ? Color.black : Color.white
+                 }else{
+                     if isCapsule {
+                         Rectangle().fill(.ultraThinMaterial).clipShape(Capsule())
+                     }
+                     else {
+                         RoundedRectangle(cornerRadius: cornerRadius).fill(.ultraThinMaterial)
+                     }
                 }
-            .background {
-                if contrast != .increased {
-                    AnyView(Rectangle().fill(.ultraThinMaterial))
-                } else {
-                    AnyView(EmptyView())
+            }
+            .overlay {
+                Group {
+                    if isCapsule {
+                        Capsule()
+                            .stroke(
+                                colorScheme == .dark ? Color.white.opacity(0.08) : Color.black.opacity(0.3), lineWidth: contrast == .increased ? 2 : 0.5
+                            )
+                    } else {
+                        RoundedRectangle(cornerRadius: cornerRadius)
+                            .stroke(
+                                    colorScheme == .dark ? Color.white.opacity(0.19) : Color.black.opacity(0.16),lineWidth: contrast == .increased ? 2 : 0.5
+                            )
+                        
+                    }
                 }
             }
             .cornerRadius(8)
             }
         }
+extension View {
+    func glassCardStyle(cornerRadius: CGFloat = 16, isCapsule: Bool = false) -> some View {
+        self.modifier(GlassCardModifier(cornerRadius: cornerRadius, isCapsule: isCapsule))
+    }
+}
 
     
 
